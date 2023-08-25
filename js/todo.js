409c33a3d74e913ed4d41dd68017b21aef98582b
이승민 <smlee@duranno.com>
2023-08-25 14:14:44 +0900

    TO DO 리스트 실습2

--- js/todo.js
+++ js/todo.js
@@ -2,21 +2,27 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
-const todos  = [];
+const TODOS_KEY = "todos";
 
-function savetTodo(){
-    localStorage.setItem("todos", JSON.stringify(todos));
+let todos  = [];
+
+
+function saveTodo(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
 
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove();
+    todos = todos.filter((item) => item.id !== parseInt(li.id));
+    saveTodo();
 }
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteTodo);
@@ -29,10 +35,25 @@ function handleSubmit(event){
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-    todos.push(newTodo);
-    paintToDo(newTodo);
-    savetTodo();
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    console.log(newTodoObj);
+    todos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveTodo();
 }
 
 todoForm.addEventListener("submit", handleSubmit);
+
+const savedTodo = localStorage.getItem(TODOS_KEY);
+
+if(savedTodo !== null){
+    const parsedToDos = JSON.parse(savedTodo);
+    todos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+
 
